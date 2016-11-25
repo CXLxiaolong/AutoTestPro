8501843d629f91c0c2021708c6299dd6c53e827f
Xiaolong <m15701007635@163.com>
2016-11-25 15:03:03 +0800

    First commit

---
+++ PageObject/HomePage.cs
@@ -0,0 +1,22 @@
+﻿using MyDriver;
+using OpenQA.Selenium;
+
+
+namespace PageObject
+{
+    public class HomePage
+    {
+        private readonly WebDriver _driver;
+        // 构造 函数 , 再类中传递参数
+       
+        public WebElement ContactUS
+        {
+            get
+            {
+                var element = _driver.FindElement(By.XPath("//a[text()='CONTACT US']"));
+                return element;
+               
+            }
+        }
+    }
+}
---
+++ PageObject/Pages.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using MyDriver;
+
+namespace PageObject
+{
+  public static class Pages
+    {
+      
+        private static readonly WebDriver _driver;
+
+      //  public Pages(WebDriver driver)
+       // {
+         //   _driver = driver;
+        // }
+
+        public static HomePage homepage
+        {
+            get{
+
+                var Page = new HomePage();
+                return Page;          
+            }
+
+        }
+        
+
+
+
+
+    }
+}
